689314c49f55f00a467dd50ac99bd19de4c3e68f
Akimov Andru <werth234@yandex.ru>
2023-06-05 10:42:44 +0300

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,21 +1,15 @@
-// Исправьте функцию sendRequest
-// Аргументы функции:
-// - имя клиента
-// - телефон клиента
-// - объект с адресом доставки: {street, house, entrance, floor, flat}
-// - список товаров в заказе
-// - стоимость заказа с учетом скидок и доставки
-// Как результат функции требуется вернуть JSON,
-// cформированный в соответствии с правилами:
-// Объект data содержит все данные
-// В объекте data есть свойства:
-// - client - строка, имя клиента + телефон клиента;
-// - order - объект, содержащий данные о заказе:
-//     - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
-//     - sum - стоимость заказа с учетом скидок и доставки
-// - goods: массив объектов с информацией о позициях заказа:
-//     - title - название позиции
-//     - count - количество в заказе
+function sendRequest(name, phone, address, goods, sum) {
+    let data = {client: {name, phone}, order: {address, sum}, goods:[]};
+    let countOfGoods = goods.length;
+    for (let i = 0; i < countOfGoods; i +=1) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
+    }
+    data.order.address = "ул. " + address.street + ", дом " + address.house + " , " + address.entrance + " подъезд," + address.floor + " этаж, " + "кв. " + address.flat;    
+    data.order.sum = sum;
+    data.client = sendRequest(name) + ` ` + sendRequest(phone);
+    let jsonData = JSON.stringify({data});
+    return jsonData;
+}  
 // например:
 // {
 //    "data": {
